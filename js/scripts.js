6828d90d5081662d36e6fa57bef1c1e7269ab4d3
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-03-25 10:14:52 -0700

    add more script functions
    Co-authored-by: Min Chang <minchangmhc@gmail.com>
    Co-authored-by: Michael Burton <micbur1@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+  
   const age = parseInt(prompt("Enter Your Age"));
 
-  if (age => 18) {
-  $('#eligible').show();
-  } else {
-  $('#Ineligible').show();  
-  }
+  if (age >= 18) {
+    $('#eligible').show();
+    } else if (age < 18){
+    $('#Ineligible').show();  
+    } else {
+    alert("Please enter a number.")
+    }
 });
